64f291a924441dec70dea97ad413f99eed5a5a12
rayvens <rayvens.gear@gmail.com>
2020-11-25 22:37:32 -0400

    trying to merge 2

--- main1.cpp
+++ main1.cpp
@@ -5,8 +5,9 @@ int main(){
     std::cout<<"Hello world!\n";
     std::cout<<"test\n";
     std::cout<<"This is main branch test";
+    std::cout<<"Trying to merge with pointers branch!";
+
+    //below there should be some space and some variables/pointers!
 
     
-
-
-}
+ }
